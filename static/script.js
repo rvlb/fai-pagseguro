8233ac3b6fab58ce2276466cf9250f8b9c566781
rvlb-19 <rvlb@cin.ufpe.br>
2018-01-14 15:04:47 -0300

    Alteração no JS para tratar funcionalidades assíncronas.

--- static/script.js
+++ static/script.js
@@ -7,38 +7,53 @@ function getCookie(name) {
 var pagseguroSession = getCookie('pagseguro_session');
 PagSeguroDirectPayment.setSessionId(pagseguroSession);
 
-var creditCardToken = undefined;
-PagSeguroDirectPayment.createCardToken({
-    cardNumber: '4111111111111111',
-    cvv: '123',
-    expirationMonth: '12',
-    expirationYear: '2030',
-    success: function(response) {
-        console.log(response);
-        creditCardToken = response.card.token;
-    },
-    error: function(error) {
-        console.log(error);
-    }
-});
+function submitForm(selectedOption, creditCardToken) {
+    var form = document.getElementById('cart-form');
 
-var selectedOption = undefined;
-document.getElementById('credit-card-btn').addEventListener('click', function(e) {
-    selectedOption = 'credit-card';
-});
+    var operationType = document.createElement('input');
+    operationType.setAttribute('type', 'hidden');
+    operationType.setAttribute('name', selectedOption);
+    form.appendChild(operationType);
 
-document.getElementById('cart-form').addEventListener('submit', function(e) {
     var senderHashInput = document.createElement('input');
     senderHashInput.setAttribute('type', 'hidden');
     senderHashInput.setAttribute('name', 'sender-hash');
     senderHashInput.setAttribute('value', PagSeguroDirectPayment.getSenderHash());
-    this.appendChild(senderHashInput);
+    form.appendChild(senderHashInput);
 
     if(selectedOption === 'credit-card') {
         var creditCardTokenInput = document.createElement('input');
         creditCardTokenInput.setAttribute('type', 'hidden');
         creditCardTokenInput.setAttribute('name', 'card-token');
         creditCardTokenInput.setAttribute('value', creditCardToken);
-        this.appendChild(creditCardTokenInput);
+        form.appendChild(creditCardTokenInput);
     }
+    form.submit();
+}
+
+document.getElementById('bank-slip-btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('credit-card-btn').setAttribute('disabled', true);
+    this.setAttribute('disabled', true);
+    submitForm('bank-slip');
+});
+
+document.getElementById('credit-card-btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.getElementById('bank-slip-btn').setAttribute('disabled', true);
+    this.setAttribute('disabled', true);
+    PagSeguroDirectPayment.createCardToken({
+        cardNumber: '4111111111111111',
+        cvv: '123',
+        expirationMonth: '12',
+        expirationYear: '2030',
+        success: function(response) {
+            console.log(response);
+            var creditCardToken = response.card.token;
+            submitForm('credit-card', creditCardToken);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
 });
